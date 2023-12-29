65ea2ffe429a551714e0a780de1ea56db8163f0a
nainoktv <nainoaktvillegas@gmail.com>
2023-12-28 21:25:22 -0800

    chore: add more models and import content types

--- MukiMuscle/main/models.py
+++ MukiMuscle/main/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 class UserProfile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -36,4 +39,33 @@ class Like(models.Model):
 class Follow(models.Model):
   follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
   following = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+  timestamp = models.DateTimeField(auto_new_add=True)
+
+class Hashtag(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+
+class PostHashtag(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+
+class FitnessChallenge(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  start_date = models.TextField()
+  end_date = models.TextField()
+
+class Chat(models.Model):
+  participants = models.ManyToManyField(User, related_name="chats")
+
+class Message(models.Model):
+  chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+  sender = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
+  timestamp = models.DateTimeField(auto_new_add=True)
+
+class Activity(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey("content_type", "object_id")
   timestamp = models.DateTimeField(auto_new_add=True)
