0e0cc92c25d48fe21e5fc9a7e7b4ac4fa54042ce
nainoktv <nainoaktvillegas@gmail.com>
2024-01-01 00:06:06 -0800

    feat: create post form

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('profile/<username>/', views.profile, name='profile'),
     path('post/', views.post_detail, name='post_detail'),
+    path('post/create/', views.create_post, name='create_post'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+
 ]
-    # path('post/<int:post_id>/', views.post_detail, name='post_detail'),
--- main/views.py
+++ main/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, UserProfile, Recipe
 from django.contrib.auth.models import User
+from .forms import PostForm
 
 def home(request):
-    # posts = Post.object.all()
-    return render(request, 'main/home.html')
-    # return render(request, 'main/home.html', {'posts': posts})
+    posts = Post.objects.all().order_by("-timestamp")
+    return render(request, 'main/home.html', {'posts': posts})
 
 # TODO: Add username response to profile
 # def profile(request, username):
 #     user = get_object_or_404(User, username=username)
 #     user_profile = get_object_or_404(UserProfile, user=user)
-#     return render(request, 'profile/profile_detail.html', {"user_profile": user_profile})
+#     return render(request, 'profile/profile_detail.html', {"user_profile": user_profile, "user": user})
 
 def profile(request, username):
     print(f"Requested username: {username}")
@@ -19,9 +19,22 @@ def profile(request, username):
     print(f"User found: {user.username}")
     return render(request, 'profile/profile_detail.html', {"user": user})
 
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect("home")
 
-# TODO: Add post_id response to post_detail
-def post_detail(request):
-    return render(request, 'post/post_detail.html')
-    # post = get_object_or_404(Post, id=post_id)
-    # return render(request, 'post/post_detail.html', {'post': post})
+    else:
+        form = PostForm()
+        
+    return render(request, "post/create_post.html", {"form": form})
+
+def post_detail(request, post_id):
+    # return render(request, 'post/post_detail.html')
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'post/post_detail.html', {'post': post})
+        
