c39423d4a703f36fc3efdd492bf37e6eb3b4e177
nainoktv <nainoaktvillegas@gmail.com>
2024-01-02 20:47:58 -0800

    feat: sign up form

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,18 @@
-from django import forms
 from .models import Post
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content']
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30, required=True, help_text="Required.")
+    last_name = forms.CharField(max_length=30, required=True, help_text="Required.")
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
