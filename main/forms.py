0ce59fb36d9ee04fa8918cf2eb732cd7f946aa02
nainoktv <nainoaktvillegas@gmail.com>
2024-01-12 23:09:46 -0800

    initial commit: avatar upload to userprofile

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,8 @@
-from .models import Post
 from django.contrib.auth.models import User
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.files.images import get_image_dimensions
+from django import forms
+from .models import Post, UserProfile
 
 
 class PostForm(forms.ModelForm):
@@ -23,3 +24,35 @@ class SignupForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio', 'avatar']
+
+    def clean_avatar(self):
+        avatar = self.cleaned_data['avatar']
+
+        try:
+            w, h = get_image_dimensions(avatar)
+
+            max_width = max_height = 100
+            if w > max_width or h > max_height:
+                raise forms.ValidationError(
+                    u'Please use an image that is '
+                    '%s x %s pixels or smaller.' % (max_width, max_height))
+            
+            main, sub = avatar.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG , '
+                    'GIF or PNG image.')
+            
+            if len(avatar) > (20 * 1024):
+                raise forms.ValidationError(
+                    u'Avatar file size may not exceed 20k. ')
+        
+        except AttributeError:
+
+            pass
+
+        return avatar
